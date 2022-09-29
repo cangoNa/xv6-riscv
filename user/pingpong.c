c556c5816004871a465fcc77ee156ed1ee111222
Your Name <you@example.com>
2022-09-29 13:37:54 +0900

    write coments pingpong.c

--- user/pingpong.c
+++ user/pingpong.c
@@ -5,17 +5,18 @@
 int
 main(int argc, char *argv[])
 {
+	// create pipe
 	int p[2];
 	pipe(p);
+	
 	char buf[32];
 
+	// child process
 	if(fork() == 0) {
 		int pid;
 		pid = getpid();
 
-		close(0);
-		dup(p[0]);
-
+		// read -> print -> write back
 		read(p[0], buf, sizeof(buf));
 		printf("<%d>: received ping\n", pid);
 		write(p[1], "pong\n", 5);
@@ -23,10 +24,12 @@ main(int argc, char *argv[])
 		close(p[0]);
 		close(p[1]);
 	}
+	// parent process
 	else {
 		int pid;
 		pid = getpid();
 
+		// write -> read -> print
 		write(p[1], "ping\n", 5);
 		read(p[0], buf, sizeof(buf));
 		printf("<%d>: received pong\n", pid);
